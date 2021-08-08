be84f57140220c49cb78db9920975557aa1855d2
m3k0813 <m3k0813@gmail.com>
2021-08-08 18:04:16 +0900

    Fragment

--- app/src/main/java/com/example/korarmy/LoginActivity.java
+++ app/src/main/java/com/example/korarmy/LoginActivity.java
@@ -2,26 +2,102 @@ package com.example.korarmy;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 
-    TextView textView_get;
+public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+
+    private SignInButton googlebtn;
+    private FirebaseAuth auth;
+    private GoogleApiClient googleApiClient;
+    private static final int REQ_SIGH_GOOGLE = 100; // 구글 로그인 결과 코드
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        textView_get = findViewById(R.id.textView_get);
 
-        Intent intent = getIntent();
-        Bundle bundle = intent.getExtras();
-        String email = bundle.getString("email");
-        String pwd = bundle.getString("pwd");
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
 
-        textView_get.setText(email + "/" + pwd);
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, googleSignInOptions)
+                .build();
+
+        auth = FirebaseAuth.getInstance();
+
+        googlebtn = findViewById(R.id.googlebtn);
+        googlebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+                startActivityForResult(intent, REQ_SIGH_GOOGLE);
+            }
+        });
+    }
+
+        // 구그 로그인 인증 요청 했을 떄 결과 값을 되돌려 받는 곳
+        @Override
+        protected void onActivityResult ( int requestCode, int resultCode, @Nullable Intent data){
+            super.onActivityResult(requestCode, resultCode, data);
+
+            if (requestCode == REQ_SIGH_GOOGLE) {
+                GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+                if (result.isSuccess()) {
+                    GoogleSignInAccount account = result.getSignInAccount();
+                    resultLogin(account);
+                }
+            }
+        }
+
+
+        private void resultLogin(GoogleSignInAccount account) {
+            AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+            auth.signInWithCredential(credential)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(LoginActivity.this, "로그인 성공", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+//                            intent.putExtra("name", account.getDisplayName());
+
+                                startActivity(intent);
+                            } else {
+                                Toast.makeText(LoginActivity.this, "로그인 실패", Toast.LENGTH_SHORT).show();
+
+                            }
+                        }
+                    });
+        }
+
+        @Override
+        public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
--- app/src/main/java/com/example/korarmy/MainActivity.java
+++ app/src/main/java/com/example/korarmy/MainActivity.java
@@ -1,136 +1,92 @@
 package com.example.korarmy;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-import android.content.Intent;
+
 import android.os.Bundle;
-import android.view.View;
+import android.view.MenuItem;
 import android.widget.Button;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.dynamic.IFragmentWrapper;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.textfield.TextInputEditText;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.GoogleAuthProvider;
+import com.example.korarmy.frag.Frag1;
+import com.example.korarmy.frag.Frag2;
+import com.example.korarmy.frag.Frag3;
+import com.example.korarmy.frag.Frag4;
 
-public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-    TextInputEditText TextInput_email, TextInput_pwd;
-    TextView findpwdbtn, signupbtn;
-    Button loginbtn;
-    private SignInButton googlebtn;
-    private FirebaseAuth auth;
-    private GoogleApiClient googleApiClient;
-    private static final int REQ_SIGH_GOOGLE = 100; // 구글 로그인 결과 코드
+public class MainActivity extends AppCompatActivity {
+
+    private Button login_btn;
+    private BottomNavigationView bottomNavigationView;
+    private FragmentManager fm;
+    private FragmentTransaction ft;
+    private Frag1 frag1;
+    private Frag2 frag2;
+    private Frag3 frag3;
+    private Frag4 frag4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        TextInput_email = findViewById(R.id.TextInput_email);
-        TextInput_pwd = findViewById(R.id.TextInput_pwd);
-        findpwdbtn = findViewById(R.id.findpwdbtn);
-        signupbtn = findViewById(R.id.signupbtn);
-
-        loginbtn = findViewById(R.id.loginbtn);
-        loginbtn.setOnClickListener(new View.OnClickListener() {
+        bottomNavigationView = findViewById(R.id.bottomNavi);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View v) {
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_fisrt:
+                        setFrag(0);
+                        break;
 
-                String email = TextInput_email.getText().toString();
-                String pwd = TextInput_pwd.getText().toString();
+                    case R.id.action_second:
+                        setFrag(1);
+                        break;
 
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                intent.putExtra("email", email);
-                intent.putExtra("pwd", pwd);
-                startActivity(intent);
+                    case R.id.action_third:
+                        setFrag(2);
+                        break;
+
+                    case R.id.action_four:
+                        setFrag(3);
+                        break;
+
+                }
+                return true;
             }
         });
-        signupbtn.setClickable(true);
-        signupbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
-                startActivity(intent);
-            }
-        });
-
-
-        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
-
-        googleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this, this)
-                .addApi(Auth.GOOGLE_SIGN_IN_API, googleSignInOptions)
-                .build();
-
-        auth = FirebaseAuth.getInstance();
-
-        googlebtn = findViewById(R.id.googlebtn);
-        googlebtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
-                startActivityForResult(intent, REQ_SIGH_GOOGLE);
-            }
-        });
+        frag1 = new Frag1();
+        frag2 = new Frag2();
+        frag3 = new Frag3();
+        frag4 = new Frag4();
+        setFrag(0);
     }
 
-    // 구그 로그인 인증 요청 했을 떄 결과 값을 되돌려 받는 곳
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == REQ_SIGH_GOOGLE) {
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            if (result.isSuccess()) {
-                GoogleSignInAccount account = result.getSignInAccount();
-                resultLogin(account);
-            }
+    // 프레그 이동
+    private void setFrag(int n) {
+        fm = getSupportFragmentManager();
+        ft = fm.beginTransaction();
+        switch (n) {
+            case 0 :
+                ft.replace(R.id.main_frame, frag1);
+                ft.commit();
+                break;
+            case 1 :
+                ft.replace(R.id.main_frame, frag2);
+                ft.commit();
+                break;
+            case 2 :
+                ft.replace(R.id.main_frame, frag3);
+                ft.commit();
+                break;
+            case 3 :
+                ft.replace(R.id.main_frame, frag4);
+                ft.commit();
+                break;
         }
-    }
-
-    private void resultLogin(GoogleSignInAccount account) {
-        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
-        auth.signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            Toast.makeText(MainActivity.this, "로그인 성공", Toast.LENGTH_SHORT).show();
-                            Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
-                            intent.putExtra("name", account.getDisplayName());
-
-                            startActivity(intent);
-                        } else {
-                            Toast.makeText(MainActivity.this, "로그인 실패", Toast.LENGTH_SHORT).show();
-
-                        }
-                    }
-                });
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
     }
 }
