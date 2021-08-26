9638ba75a1487ea6984f65b7e4c1f2797d3641a0
m3k0813 <m3k0813@gmail.com>
2021-08-26 17:01:53 +0900

    SplashActivity

--- app/src/main/java/com/example/korarmy/SplashActivity.java
+++ app/src/main/java/com/example/korarmy/SplashActivity.java
@@ -2,6 +2,7 @@ package com.example.korarmy;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,6 +17,21 @@ public class SplashActivity  extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
 
 
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                MyShardPreferences();
+                finish();
+            }
+        },3000);
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+    public void MyShardPreferences(){
         if (MySharedPreferences.getUserEamil(this).length() != 0) {
             Intent intent = new Intent(getApplicationContext(), MainActivity.class);
             startActivity(intent);
